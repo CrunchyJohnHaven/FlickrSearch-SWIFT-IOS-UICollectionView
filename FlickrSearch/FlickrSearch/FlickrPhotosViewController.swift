22dbde52d2b5572214417c5e786053bb558840fe
John Bradley <johnhavenbradley@gmail.com>
2018-03-18 18:54:20 -0700

    update

--- FlickrSearch/FlickrSearch/FlickrPhotosViewController.swift
+++ FlickrSearch/FlickrSearch/FlickrPhotosViewController.swift
@@ -7,7 +7,7 @@
 //
 //073dd0bb772360865139e096b92044e5
 //4f3400baf62994d6
-
+//http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=073dd0bb772360865139e096b92044e5&format=rest&api_sig=f24f4e98063a9b8ecc8b522b238 d5e2f
 
 
 
@@ -18,5 +18,118 @@ final class FlickrPhotosViewController: UICollectionViewController {
     // MARK: - Properties
     fileprivate let reuseIdentifier = "FlickrCell"
     fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
-    
+    //Create an array of flickrSearchResults
+    fileprivate var searches = [FlickrSearchResults]()
+    fileprivate let flickr = Flickr()
+    fileprivate let itemsPerRow: CGFloat = 3
+
 }
+//photoForIndexPath ->
+private extension FlickrPhotosViewController {
+    func photoForIndexPath(indexPath: IndexPath) -> FlickrPhoto {
+        return searches[(indexPath as NSIndexPath).section].searchResults[(indexPath as IndexPath).row]
+    }
+}
+//extension to hold the text field delegate methods
+extension FlickrPhotosViewController : UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // 1
+//        add an activity indicator
+        let activityIndicator = UIActivityIndicatorView(activityIndicatorStyle: .gray)
+        textField.addSubview(activityIndicator)
+        activityIndicator.frame = textField.bounds
+        activityIndicator.startAnimating()
+        
+        flickr.searchFlickrForTerm(textField.text!) {
+            //log any errors to the console
+            results, error in
+            
+            
+            activityIndicator.removeFromSuperview()
+            
+            
+            if let error = error {
+                // 2
+                print("Error searching : \(error)")
+                return
+            }
+            
+            if let results = results {
+                // 3
+                print("Found \(results.searchResults.count) matching \(results.searchTerm)")
+                self.searches.insert(results, at: 0)
+                
+                // 4
+                self.collectionView?.reloadData()
+            }
+        }
+        
+        textField.text = nil
+        textField.resignFirstResponder()
+        return true
+    }
+}
+// MARK: - UICollectionViewDataSource
+extension FlickrPhotosViewController {
+    //1
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return searches.count
+    }
+    
+    //2
+    override func collectionView(_ collectionView: UICollectionView,
+                                 numberOfItemsInSection section: Int) -> Int {
+        return searches[section].searchResults.count
+    }
+    
+    //3
+    override func collectionView(_ collectionView: UICollectionView,
+                                 cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier,
+                                                      for: indexPath)
+        cell.backgroundColor = UIColor.black
+        // Configure the cell
+        return cell
+    }
+}
+
+//Allow the view controller to conform to the flow layout delegate protocol
+extension FlickrPhotosViewController : UICollectionViewDelegateFlowLayout {
+    //1
+    //responsible for telling the layout the size of a given cell
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        //2
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    //3
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    // 4
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
+
+
+
+
+
+
+
+
+
+
+

86618c76ca99b8667fe3cfcaa967f7986c59879a
John Bradley <johnhavenbradley@gmail.com>
2018-03-18 18:27:11 -0700

    update

---
+++ FlickrSearch/FlickrSearch/FlickrPhotosViewController.swift
@@ -0,0 +1,22 @@
+//
+//  FlickrPhotosViewController.swift
+//  FlickrSearch
+//
+//  Created by john bradley on 3/18/18.
+//  Copyright © 2018 john. All rights reserved.
+//
+//073dd0bb772360865139e096b92044e5
+//4f3400baf62994d6
+
+
+
+
+import UIKit
+
+final class FlickrPhotosViewController: UICollectionViewController {
+    
+    // MARK: - Properties
+    fileprivate let reuseIdentifier = "FlickrCell"
+    fileprivate let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    
+}
